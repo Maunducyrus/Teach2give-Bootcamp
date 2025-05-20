69a38dc24cd68a004e2be0b188efd3ad0754d172
Maunducyrus <maunducyrus123@gmail.com>
2025-05-20 18:26:24 +0300

    Completed Challenge 2: Convert Minutes into Seconds

--- Javascript-Challenges/milestone_1_challenges.js
+++ Javascript-Challenges/milestone_1_challenges.js
@@ -10,3 +10,13 @@ let sum1 = addition(3, 5);
 let sum2 = addition(-6, 9);
 console.log(sum1); // 8
 console.log(sum2); // 3
+
+
+// Challenge 2: Convert Minutes into Seconds
+function convert (minutes) {
+    return minutes * 60;
+}
+let seconds1 = convert(5);
+let seconds2 = convert(2);
+console.log(seconds1); // 300
+console.log(seconds2); // 180
