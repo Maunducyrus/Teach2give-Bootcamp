26970f50827e2bad32a4c2c718fbf5695c6f57af
Maunducyrus <maunducyrus123@gmail.com>
2025-05-20 18:18:43 +0300

    Completed Challenge 1: Sum of Even Numbers

---
+++ Javascript-Challenges/milestone_1_challenges.js
@@ -0,0 +1,12 @@
+// JavaScript Challenge Exercise
+
+// Challenge 1: Return the sum of two numbers
+
+function addition (num1, num2) {
+    return num1 + num2;
+}
+
+let sum1 = addition(3, 5);
+let sum2 = addition(-6, 9);
+console.log(sum1); // 8
+console.log(sum2); // 3
