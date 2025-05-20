c8843e443098016f078521ec2d2bbbfae529ace6
Maunducyrus <maunducyrus123@gmail.com>
2025-05-20 18:37:50 +0300

    Completed Challenge 3: Perimeter of a Rectangle

--- Javascript-Challenges/milestone_1_challenges.js
+++ Javascript-Challenges/milestone_1_challenges.js
@@ -20,3 +20,14 @@ let seconds1 = convert(5);
 let seconds2 = convert(2);
 console.log(seconds1); // 300
 console.log(seconds2); // 180
+
+
+// Challenge 3: Perimeter of a Rectangle
+function perimeterOfRectangle (length, width) {
+    return 2 * (length + width);
+}
+let perimeter1 = perimeterOfRectangle(6, 7); // 26
+let perimeter2 = perimeterOfRectangle(20, 10); // 60
+console.log(perimeter1);
+console.log(perimeter2);
+
