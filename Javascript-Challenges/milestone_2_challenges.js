f3db4658751a49ff440404243f2ba37835a9e71a
Maunducyrus <maunducyrus123@gmail.com>
2025-05-23 15:33:11 +0300

    Completed   // Challenge 12: Reverse Linear Search

--- Javascript-Challenges/milestone_2_challenges.js
+++ Javascript-Challenges/milestone_2_challenges.js
@@ -137,3 +137,15 @@ function  average(numbers) {
   }
   console.log(linearSearch([5, 3, 7, 1, 4], 7)) // returns: 2
   console.log(linearSearch([5, 3, 7, 1, 4], 10)) // returns: -1 
+
+  // Challenge 12: Reverse Linear Search
+  function reverseLinearSearch(arr, value) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+      if (arr[i] === value) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  console.log(reverseLinearSearch([5, 3, 7, 1, 4, 7], 7)) // returns: 5
+  console.log(reverseLinearSearch([5, 3, 7, 1, 4], 10)) // returns: []
