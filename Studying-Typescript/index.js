ba0a23b15c51f021d453e556b54cb009bb3f34ba
Maunducyrus <maunducyrus123@gmail.com>
2025-06-01 15:16:23 +0300

    learning ts

--- Studying-Typescript/index.js
+++ Studying-Typescript/index.js
@@ -1,2 +1,12 @@
-// var username = "Cyrus@2024";
-// console.log(username);
+// let username: string = "Cyrus@2024";
+// console.log(username)
+var num1 = 55;
+var num2 = 77.55;
+var num3 = Infinity;
+console.log(num1, num2, num3);
+var str1 = "Cyrus";
+var str2 = "Maundu Cyrus";
+console.log("My name is".concat(str2));
+var isMale = true;
+var isFemale = false;
+console.log(isMale);
--- Studying-Typescript/index.ts
+++ Studying-Typescript/index.ts
@@ -1,2 +1,23 @@
 // let username: string = "Cyrus@2024";
 // console.log(username)
+let num1: number = 55;
+let num2: number = 77.55;
+let  num3: number = Infinity;
+console.log(num1, num2, num3);
+
+let str1: string = "Cyrus";
+let str2: string = `Maundu Cyrus`;
+console.log("My name is".concat(str2))
+
+const isMale: boolean = true;
+const isFemale: boolean = false;
+console.log(isMale);
+
+// let myVar = null;
+// myVar = 55;
+// myVar = "str"
+
+let mayVar: any = 55;
+mayVar = "Heiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiillo there"
+myVar = true
+mayVar uns
